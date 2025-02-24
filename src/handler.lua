e424d80ef622170e11214519ebd3b013c0fa8a6f
Julien Pelletier <construire.usine6t@icloud.com>
2025-02-24 14:46:12 +0100

    Fix version field in kong-oidc-auth plugin

--- src/handler.lua
+++ src/handler.lua
@@ -3,6 +3,7 @@ local access = require "kong.plugins.kong-oidc-auth.access"
 local KongOidcAuth = {}
 
 KongOidcAuth.PRIORITY = 1000
+KongOidcAuth.VERSION = "0.3.0"
 
 function KongOidcAuth:access(conf)
 	access.run(conf)
